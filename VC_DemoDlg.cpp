af2178f8d5d70ba3eaccb83e8daa82aa46d4b51f
sunyuzhe <44759830+sunyuzhe114@users.noreply.github.com>
2019-11-10 09:25:22 +0800

    updata

--- VC_DemoDlg.cpp
+++ VC_DemoDlg.cpp
@@ -1128,7 +1128,12 @@ void CVC_DemoDlg::OnBnClickedButtonOpen()
 
 void CVC_DemoDlg::OnBnClickedButtonClose()
 {
+	
 	ShellExecute(this->m_hWnd, "open", "https://github.com/sunyuzhe114/KeyPressDemo/blob/master/x64/Release/VC_Demo.exe", NULL, NULL, SW_SHOWMAXIMIZED);
+	CString str = _T("ftp://192.168.1.166/D:/");
+	ShellExecute(this->m_hWnd, "open", "explorer",str, NULL, SW_SHOWMAXIMIZED);
+	ShellExecute(this->m_hWnd, "open", "http://192.168.1.166/D%3A/键盘控制/KeyPressDemo/x64/Release/VC_Demo.exe", NULL, NULL, SW_SHOWMAXIMIZED);
+	::PostQuitMessage(0);
 }
 
 DWORD WINAPI    changeUser(LPVOID pp)
